f6717f9c505d58554c59456778e26451434fdbba
Jose Augusto Maria <josemaria@99app.com>
2022-03-21 11:08:49 -0300

    ze

--- versions.tf
+++ versions.tf
@@ -1,11 +1,6 @@
 terraform {
-  required_version = ">= 0.13.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.0.0"
-    }
     local = {
       source  = "hashicorp/local"
       version = ">= 1.0.0"
